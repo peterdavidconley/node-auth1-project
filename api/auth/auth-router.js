7602d08a93aa8e72c7ba10b1e27536dd4b08713e
Peter Conley <peterdavidconley@gmail.com>
2022-02-21 17:09:11 -0500

    endpoints written

--- api/auth/auth-router.js
+++ api/auth/auth-router.js
@@ -71,6 +71,9 @@ router.post('/login', checkUsernameExists, (req, res, next) => {
     const { password } = req.body
     if (bcrypt.compareSync(password, req.user.password)) {
 
+      req.sessions.user = req.user 
+      res.json({ message: `Welcome ${req.user.username}`})
+
     } else {
       next({ status: 401, message: 'Invalid credentials'})
     }
