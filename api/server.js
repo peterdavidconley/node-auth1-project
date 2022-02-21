bab07cbfe4b6991e04f2e44da10f69f86d8dea91
Peter Conley <peterdavidconley@gmail.com>
2022-02-21 15:08:25 -0500

    wrote scaffolding for authRouter.js

--- api/server.js
+++ api/server.js
@@ -2,6 +2,8 @@ const express = require("express");
 const helmet = require("helmet");
 const cors = require("cors");
 const usersRouter = require('./users/users-router');
+const authRouter = require('./auth/auth-router');
+
 
 /**
   Do what needs to be done to support sessions with the `express-session` package!
@@ -24,6 +26,8 @@ server.use(express.json());
 server.use(cors());
 
 server.use('/api/users', usersRouter);
+server.use('/api/users', authRouter);
+
 
 server.get("/", (req, res) => {
   res.json({ api: "up" });
